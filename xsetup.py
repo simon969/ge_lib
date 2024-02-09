028b52e7b8d2dbc127a875f9b1d508c203db37dc
Thomson <Simon.Thomson@aecom.com>
2024-02-09 10:57:16 +0000

    plxscripting updated to plxscripting_py311

---
+++ xsetup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='AECOM UK Ground Engineering Function Library',
+    url='https://github.com/simon969/ge_lib',
+    author='Simon Thomson',
+    author_email='simon.thomson@aecom.com',
+    # Needed to actually package something
+    # packages=['found'],
+    package_dir={'': 'src/ge_lib'},
+    # py_modules = ["module_1", "module_2"],
+    # Needed for dependencies
+    install_requires=["importlib-metadata",
+                    "packaging",
+                    "pypyodbc",
+                    "pandas",
+                    "numpy",
+                    "numpy-utils",
+                    "python-AGS4",
+                    "tomli",
+                    "zipp",
+                    "ipykernel",
+                    "dict2xml",
+                    "pycryptodome",
+                    "chardet",
+                    "matplotlib",
+                    "qtconsole",
+                    "urllib3",
+                    "requests"],
+    # unixodbc is needed for pypyodbc on linux system  (https://github.com/mkleehammer/pyodbc/issues/36)
+    
+    # *strongly* suggested for sharing
+    version='0.0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Python code library for ground engineering',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
+)
